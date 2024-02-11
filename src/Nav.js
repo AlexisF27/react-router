94a672a2a1c7565843fd4d61b72f1d9356dc5c39
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-02-11 02:10:37 +0000

    Nav component with 3 links to routes (Link)

--- src/Nav.js
+++ src/Nav.js
@@ -1,16 +1,25 @@
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
-const Nav = ({search, setSearch}) => {
+const Nav = ({ search, setSearch }) => {
     return (
-        <nav className="nav">
+        <nav className="Nav">
             <form className="searchForm" onSubmit={(event) => event.preventDefault()}>
                 <label htmlFor="search">Search Posts</label>
                 <input
-                id="search"
-                type="text"
-                placeholder="Search Posts"
-                value={search}
-                onChange={(e) => setSearch(e.target.value)}></input>
+                    id="search"
+                    type="text"
+                    placeholder="Search Posts"
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                ></input>
             </form>
+            <ul>
+                <li>
+                    <Link to="/"> Home</Link>
+                    <Link to="/post"> Post</Link>
+                    <Link to="/about"> About</Link>
+                </li>
+            </ul>
         </nav>
     )
 }
